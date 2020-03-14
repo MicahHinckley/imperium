3d9bc290fdbcdb5c84e1b01d2826c028a22f7ece
Micah Hinckley <hinckleytmicah@gmail.com>
2020-03-14 13:00:30 -0700

    Update cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::process::Command;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -8,5 +9,20 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    
+
+    if args.command == "new" {
+        let mut directory = String::from("/");
+        directory.push_str(&args.name);
+
+        let result = std::fs::create_dir(directory);
+        match result {
+            Ok(_) => {},
+            Err(error) => { println!("{}", error); }
+        }
+
+        // let path = std::fs::canonicalize(format!("../{}", &args.name));
+        let mut cmd = Command::new("git");
+        cmd.current_dir(format!("./{}", &args.name));
+        cmd.arg("init");
+    }
 }
