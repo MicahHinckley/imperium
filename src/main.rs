f38c7aa323ee398dcefe7b73d00537d0003f00b6
Micah Hinckley <hinckleytmicah@gmail.com>
2020-03-17 20:54:57 -0700

    Update cli

--- src/main.rs
+++ src/main.rs
@@ -26,19 +26,32 @@ fn try_add_dependency(path: &Path) -> Result<(), Error> {
 fn new(name: &str) -> Result<(), Error> {
     let path = format!("./{}", name);
     let base_path = Path::new(&path);
+
+    println!("{}", base_path.display());
+
     fs::create_dir_all(&base_path)?;
+
+    println!("{}", base_path.display());
 
     try_git_init(&base_path)?;
     try_add_dependency(&base_path)?;
 
+    println!("{}", base_path.display());
+
     let src = base_path.join("src");
     fs::create_dir_all(&src)?;
+
+    println!("{}", base_path.display());
 
     let src_shared = src.join("shared");
     fs::create_dir_all(src.join(&src_shared))?;
 
+    println!("{}", base_path.display());
+
     let src_server = src.join("server");
     fs::create_dir_all(src.join(&src_server))?;
+
+    println!("{}", base_path.display());
 
     let src_client = src.join("client");
     fs::create_dir_all(src.join(&src_client))?;
