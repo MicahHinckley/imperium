8f990687fe7d9e4271c8669162b75208cd45ddb9
Micah Hinckley <hinckleytmicah@gmail.com>
2020-09-28 20:05:32 -0700

    Finish log

--- framework/shared/Log/init.lua
+++ framework/shared/Log/init.lua
@@ -1,44 +1,98 @@
---< Modules >--
-local Display = require(script.Display)
-
 --< Functions >--
-local function BetterFormat(template, ...)
-    local Values = {...}
-    
-    local Matches = 0
-    local Result = template:gsub("{([^}]*)}", function(args)
-        Matches = Matches + 1
+local function WriteToBuffer(buffer, value)
+    local LastValue = buffer[#buffer]
 
-        local Value = Values[Matches]
+    if type(value) == "string" and type(LastValue) == "string" then
+        buffer[#buffer] = LastValue .. value
+    else
+        table.insert(buffer, value)
+    end
+end
 
-        if args == "?" then
-            return Display(Value)
-        elseif args == "" then
-            return tostring(Value)
+local function DebugRepresentation(buffer, value)
+    local Type = typeof(value)
+
+    if Type == "string" then
+        WriteToBuffer(buffer, string.format("%q", value))
+    elseif Type == "table" then
+        local Metatable = getmetatable(value)
+
+        if Metatable ~= nil and Metatable.__fmtDebug ~= nil then
+            WriteToBuffer(buffer, Metatable.__fmtDebug(value))
         else
-            error(string.format("Invalid format string %q", args))
+            WriteToBuffer(buffer, value)
         end
-    end)
-    
-    return Result
+    elseif Type == "Instance" then
+        WriteToBuffer(buffer, value:GetFullName())
+    else
+        WriteToBuffer(buffer, tostring(value))
+    end
+end
+
+local function fmt(template, ...)
+    local Buffer = {}
+
+    local CurrentArgument = 0
+    local Index = 1
+    local Length = #template
+
+    while Index <= Length do
+        local OpenBrace = string.find(template, "{", Index)
+
+        if OpenBrace == nil then
+            WriteToBuffer(Buffer, string.sub(template, Index))
+
+            break
+        else
+            local CharAfterBrace = string.sub(OpenBrace + 1, OpenBrace + 1)
+
+            if CharAfterBrace == "{" then
+                WriteToBuffer(Buffer, string.sub(template, Index, OpenBrace))
+
+                Index = OpenBrace + 2
+            else
+                if OpenBrace - Index > 0 then
+                    WriteToBuffer(Buffer, string.sub(template, Index, OpenBrace - 1))
+                end
+
+                local CloseBrace = string.find(template, "}", OpenBrace + 1)
+
+                local FormatSpecifier = string.sub(template, OpenBrace + 1, CloseBrace - 1)
+                CurrentArgument += 1
+                local Argument = select(CurrentArgument, ...)
+
+                if FormatSpecifier == "" then
+                    WriteToBuffer(Buffer, tostring(Argument))
+                elseif FormatSpecifier == ":?" then
+                    DebugRepresentation(Buffer, Argument)
+                else
+                    error("Unsupported format specifier `" .. FormatSpecifier .. "`.", 2)
+                end
+
+                Index = CloseBrace + 1
+            end
+        end
+    end
+
+    return Buffer
 end
 
 --< Module >--
 local Log = {}
 
 function Log.Error(template, ...)
-    local Message = BetterFormat(template, ...)
-    error(Message, 2)
+    local Message = fmt(template, ...)
+    error(unpack(Message), 2)
 end
 
 function Log.Warn(template, ...)
-    local Message = BetterFormat(template, ...)
-    warn(Message)
+    local Message = fmt(template, ...)
+    warn(unpack(Message))
 end
 
 function Log.Info(template, ...)
-    local Message = BetterFormat(template, ...)
-    print(Message)
+    local Message = fmt(template, ...)
+    print(unpack(Message))
 end
 
 return Log
