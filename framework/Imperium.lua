28eb23bb71b28af0063fb5185a09a4da894f4681
Micah Hinckley <hinckleytmicah@gmail.com>
2020-11-06 21:37:03 -0800

    Update Imperium.lua

--- framework/Imperium.lua
+++ framework/Imperium.lua
@@ -1,28 +1,60 @@
+--< Modules >--
+local Asink = {}
+
+--< Functions >--
+local function InitializeSystem(system)
+    local Future, Resolve = Asink.Future.new()
+
+    Asink.Runtime.exec(function()
+        system:Initialize()
+        Resolve()
+    end)
+
+    return Future
+end
+
+local function InitializeSystems(systems)
+    local Future, Resolve = Asink.Future.new()
+
+    Asink.Runtime.exec(function()
+        local SystemCache = {}
+        local InitializeJobs = {}
+
+        for _,descendant in ipairs(systems:GetChildren()) do
+            if descendant:IsA("ModuleScript") and descendant:FindFirstAncestorOfClass("ModuleScript") == nil then
+                local System = require(descendant)
+    
+                if System.Initialize ~= nil then
+                    local InitializeJob = InitializeSystem()
+
+                    table.insert(InitializeJobs, InitializeJob)
+                end
+    
+                table.insert(SystemCache, System)
+            end
+        end
+
+        Future.all(InitializeJobs):await()
+
+        Resolve(SystemCache)
+    end)
+
+    return Future
+end
+
 --< Module >--
 local Imperium = {}
 
 function Imperium:Start(systems)
-    local SystemCache = {}
-
-    for _,descendant in ipairs(systems:GetChildren()) do
-        if descendant:IsA("ModuleScript") and descendant:FindFirstAncestorOfClass("ModuleScript") == nil then
-            local System = require(descendant)
-
-            if System.Initialize then
-                System:Initialize()
+    return InitializeSystems(systems):andThen(function(systemCache)
+        for _,system in ipairs(systemCache) do
+            if system.Start ~= nil then
+                Asink.Runtime.exec(function()
+                    system:Start()
+                end)
             end
-
-            table.insert(SystemCache, System)
         end
-    end
-
-    for _,system in ipairs(SystemCache) do
-        if system.Start then
-            -- spawn thread and call start
-        end
-    end
-
-    return SystemCache
+    end)
 end
 
 return Imperium
